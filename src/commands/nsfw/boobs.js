2ada4c78b6ad429f894004311f5613c8b0defbba
Gátomo <63877602+gatomo-oficial@users.noreply.github.com>
2020-08-18 23:24:53 +0200

    (+18 commit) crear este comando :v

---
+++ src/commands/nsfw/boobs.js
@@ -0,0 +1,41 @@
+const {Command} = require('klasa'); //klasa
+const {MessageEmbed} = require('discord.js');
+
+const client = require('nekos.life'); //nekos.life
+const neko = new client(); //nekos.life
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            name: 'fuck',
+            enabled: true,
+            runIn: ['text'],
+            cooldown: 0,
+            deletable: false,
+            bucket: 1,
+            guarded: false,
+            nsfw: true,
+            permissionLevel: 0,
+            requiredPermissions: [],
+            requiredSettings: [],
+            subcommands: false,
+            description: ':v comando nsfw :v (ke palo no kiero escribir ninguna descripcion)',
+            quotedStringSupport: false,
+            extendedHelp: 'No extended help available ;-; .',
+        });
+    }
+
+    async run(message, [user]) {
+        //nekos.life
+        var boobimg = await neko.nsfw.boobs();
+
+            message.send(
+                new MessageEmbed()
+                .setImage(boobimg.url)
+                .setColor('RANDOM')
+            );
+        
+    }
+
+}
