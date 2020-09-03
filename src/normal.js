384706cd2aa80c5527720aafbf869724dd11c04c
Gátomo <63877602+gatomo-oficial@users.noreply.github.com>
2020-09-03 14:25:59 +0200

    Update normal.js

--- src/normal.js
+++ src/normal.js
@@ -9,7 +9,7 @@ new Client({
     commandEditing: true,
     typing: true,
     language: 'es-ES',
-    presence: { activity: { name: `${prefix}help • ${client.guilds.cache.size}  servers`, type: 'WATCHING' } },
+    presence: { activity: { name: `n!help ・ ${client.guilds.cache.size}  servers`, type: 'WATCHING' } },
     readyMessage: (client) => `Listo!!!! Normal está listo para estar en ${client.guilds.cache.size}  servers ;3`
 }).login(token).catch(console.error);
 
