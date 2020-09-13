01bb268b157603cb1bd805d62c2312f05910911d
David Gustavo Herrera De La Cruz <31642521+Phamzito@users.noreply.github.com>
2020-09-13 07:04:51 -0500

    chore(core): add owners

--- src/normal.js
+++ src/normal.js
@@ -9,6 +9,7 @@ new Client({
     commandEditing: true,
     typing: true,
     language: 'es-ES',
+    owners: ['497061687820812288', '685947556655923242'],
     // Mierda bug: presence: { activity: { name: `n!help ・ ${client.guilds.cache.size}  servers`, type: 'WATCHING' } },
     readyMessage: (client) => `Listo!!!! Normal está listo para estar en ${client.guilds.cache.size}  servers ;3`
 }).login(token).catch(console.error);
