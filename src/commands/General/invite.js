3e70a25120542ad65eedc38142a362f4675d90ce
Gátomo <Gatomociencia@gmail.com>
2020-10-08 21:12:39 +0200

    Update invite.js

--- src/commands/General/invite.js
+++ src/commands/General/invite.js
@@ -34,8 +34,8 @@ module.exports = class extends Command {
             new MessageEmbed()
             .setTitle(message.language.get('COMMAND_INVITE_TITLE'))
             .setDescription(message.language.get('COMMAND_INVITE_TEXT'))
-            .addField(`${message.language.get('COMMAND_INVITE_TEXT_LINK')}`, `${message.language.get('COMMAND_INVITE_LINK')}`, false)
-            .addField('Server de soporte', '[Click aquí](https://discord.gg/ZpWE57t)',false)
+            .addField(`${message.language.get('COMMAND_INVITE_TEXT_LINK')}`, `${message.language.get('COMMAND_INVITE_LINK')}`, true)
+            .addField('Server de soporte', '[Click aquí](https://discord.gg/ZpWE57t)', true)
             .setThumbnail('https://media.giphy.com/media/2yvPrTHxX6wIQM0bWT/giphy.gif')
             .setColor('RANDOM')
         );
